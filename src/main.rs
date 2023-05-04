d533425024a6a3bf55484611ad1e109a9e5600c3
zets <poi.ru0130@gmail.com>
2023-05-04 06:57:42 +0300

    simple parser link

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
-extern crate flate2;
-use std::env::args;
-use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
-use std::time::Instant;
-use flate2::Compression;
-use flate2::write::GzEncoder;
+use error_chain::error_chain;
+use select::document::Document;
+use select::predicate::Name;
 
-fn main() {
-    if args().len() != 3 {
-        eprintln!("Usage: 'source' 'target'");
-        return;
-    }
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output = File::create(args().nth(2).unwrap()).unwrap();
-    let mut encoder = GzEncoder::new(output, Compression::best());
-    let start = Instant::now();
-    copy(&mut input, &mut encoder).unwrap();
-    let output = encoder.finish().unwrap();
-    println!("Source len {:?}",
-             input.get_ref().metadata().unwrap().len());
-    println!("Target len {:?}",
-             output.metadata().unwrap().len());
-    println!("Elapsed:{:?}", start.elapsed());
+error_chain! {
+      foreign_links {
+          ReqError(reqwest::Error);
+          IoError(std::io::Error);
+      }
+}
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("http://www.rust-lang.org/en-Us")
+        .await?
+        .text()
+        .await?;
+
+    Document::from(res.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+    Ok(())
 
 }
