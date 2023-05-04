f9a35947593c882756ff1808c110f5ac18aaa971
zets <poi.ru0130@gmail.com>
2023-05-04 07:37:04 +0300

    simple web server atix Fun add and myltipy two number

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
-extern crate flate2;
-use std::env::args;
-use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
-use std::time::Instant;
-use flate2::Compression;
-use flate2::write::GzEncoder;
+use actix_web::{
+    HttpServer,
+    get,
+    App,
+    web::Path,
+    Responder,
+};
 
-fn main() {
-    if args().len() != 3 {
-        eprintln!("Usage: 'source' 'target'");
-        return;
-    }
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output = File::create(args().nth(2).unwrap()).unwrap();
-    let mut encoder = GzEncoder::new(output, Compression::best());
-    let start = Instant::now();
-    copy(&mut input, &mut encoder).unwrap();
-    let output = encoder.finish().unwrap();
-    println!("Source len {:?}",
-             input.get_ref().metadata().unwrap().len());
-    println!("Target len {:?}",
-             output.metadata().unwrap().len());
-    println!("Elapsed:{:?}", start.elapsed());
+use rhai::Engine;
+
+#[get("/multiply/{num1}/{num2}")]
+async fn multiply(path: Path<(i64, i64)>) -> impl Responder{
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/multiply.rhai".into()).unwrap();
+    format!("{result}")
+}
+#[get("/add/{num1}/{num2}")]
+async fn add(path: Path<(i64, i64)>) -> impl Responder{
+    let (num1, num2) = path.into_inner();
+    let mut engine = Engine::new();
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    let result = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
+    format!("{result}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+        HttpServer::new(||{
+                App::new()
+                    .service(multiply)
+                    .service(add)
+        })
+            .bind(("127.0.0.1", 8080))
+            .unwrap()
+            .run()
+            .await
 
 }
