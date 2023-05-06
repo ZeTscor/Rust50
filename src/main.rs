36beada5fbbe4bff34bf19d88c7577691959602a
zets <poi.ru0130@gmail.com>
2023-05-06 11:59:58 +0300

    simple std::  Vec and HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,53 @@
-extern crate flate2;
-use std::env::args;
-use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
-use std::time::Instant;
-use flate2::Compression;
-use flate2::write::GzEncoder;
+use std::collections::HashMap;
+use std::fmt::Debug;
 
 fn main() {
-    if args().len() != 3 {
-        eprintln!("Usage: 'source' 'target'");
-        return;
-    }
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output = File::create(args().nth(2).unwrap()).unwrap();
-    let mut encoder = GzEncoder::new(output, Compression::best());
-    let start = Instant::now();
-    copy(&mut input, &mut encoder).unwrap();
-    let output = encoder.finish().unwrap();
-    println!("Source len {:?}",
-             input.get_ref().metadata().unwrap().len());
-    println!("Target len {:?}",
-             output.metadata().unwrap().len());
-    println!("Elapsed:{:?}", start.elapsed());
-
+    let s2 = medium(vec![1,4,5,6,7,8,2,9,3,7,10,124,512]);
+    println!("{:?}", s2);
+    let s3 = mid(vec![1,4,5,6,7,8,2,9,3,7,10,435,436,23,56,876,45,87,90]);
+    println!("{:?}", s3);
+    let s1 = moda(vec![1,4,5,6,7,8,2,9,3,7,234,56,87,9,34]);
+    println!("{:?}", s1);
 }
+
+fn medium(mut c: Vec<i32>) -> f32{
+    let mut stac:i32 = 0;
+    let mut count:i32 = 0;
+    c.sort();
+    for mut i in &c{
+        count = count+ 1;
+        stac = stac+i;
+    };
+    let med: f32 = stac as f32/ count as f32 ;
+    med
+}
+
+fn mid(mut c: Vec<i32>) -> f32{
+    c.sort();
+    let count = c.len() as f32;
+    if c.len()%2 == 0 {
+        let mut firs_num:f32 = count/2.0;
+        let second_num = &firs_num -1.0;
+        let sum = (c[second_num as usize] as f32+c[firs_num as usize] as f32/2.0);
+        sum
+    }else {
+        let middle = c[(count/2.0) as usize];
+         middle as f32
+    }
+
+    }
+
+fn moda(mut c: Vec<i32>)->i32{
+    let mut map = HashMap::new();
+    c.sort();
+    for i in c{
+        let count = map.entry(i).or_insert(0);
+        *count+=1;
+
+    }
+    let mode = map.iter().max_by_key(|&(_, count)| count).map(|(&val, _)| val);
+    mode.unwrap()
+    }
+
+
+
